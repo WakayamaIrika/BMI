a0e1c2c78068fd7914535a3586fd4cbf11ebb68b
Irika Wakayama <i.wakayama.sys21@morijyobi.ac.jp>
2022-03-17 12:27:55 +0900

    二回繰り返します

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ public class Main{
         public static void main(String[] args){
                 Scanner s = new Scanner(System.in);
                 double sintyo,taiju,bmi;
+		int a;
+		for(a=0;a<=1;a++){
                 System.out.println("cm");
                 sintyo=s.nextDouble();
                 System.out.println("kg");
@@ -10,6 +12,7 @@ public class Main{
 		sintyo=sintyo/100;
                 bmi=taiju/(sintyo*sintyo);
 		System.out.println("BMI="+(String.format("%.1f", bmi))+"");
+		}
        }
 }
 
